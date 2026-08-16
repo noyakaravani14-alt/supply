b59914ab0c92758c9e7bc510fc35a04dfc0e245d
noyakaravani14-alt <noyakaravani14@gmail.com>
2026-08-16 14:28:57 +0300

    Update scan.js

--- js/scan.js
+++ js/scan.js
@@ -10,10 +10,23 @@ function startScan(targetInputId){
   stopScanner();
   box.style.display = 'block';
   box.innerHTML = '<div id="qr-reader" style="width:100%"></div>';
-  html5Scanner = new Html5Qrcode("qr-reader");
+  html5Scanner = new Html5Qrcode("qr-reader", {
+    formatsToSupport: [
+      Html5QrcodeSupportedFormats.EAN_13,
+      Html5QrcodeSupportedFormats.EAN_8,
+      Html5QrcodeSupportedFormats.CODE_128,
+      Html5QrcodeSupportedFormats.CODE_39,
+      Html5QrcodeSupportedFormats.CODE_93,
+      Html5QrcodeSupportedFormats.UPC_A,
+      Html5QrcodeSupportedFormats.UPC_E,
+      Html5QrcodeSupportedFormats.ITF,
+      Html5QrcodeSupportedFormats.CODABAR
+    ],
+    verbose: false
+  });
   html5Scanner.start(
     { facingMode: "environment" },
-    { fps: 10, qrbox: 220 },
+    { fps: 10, qrbox: { width: 260, height: 110 }, experimentalFeatures: { useBarCodeDetectorIfSupported: true } },
     (decodedText)=>{
       const el = document.getElementById(targetInputId);
       if(el) el.value = decodedText;
