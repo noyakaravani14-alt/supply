9b1ef2dc07929f9ca7fb58220c861bd64d6cb998
noyakaravani14-alt <noyakaravani14@gmail.com>
2026-08-17 10:20:06 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 /* expose for inline handlers */
 Object.assign(window, {setTab, promptAddLocation, editItem, saveItemEdit, openReceiveModal, onReceiveItemChange,
   onReceiveQtyChange, submitReceive, openTransferModal, onTransferChange, submitTransfer, removeUnit,
-  editUnit, saveUnitEdit, startScan, captureAndRecognize, closeModal, runHolderSearch, viewLocationHistory, submitLogin});
+  editUnit, saveUnitEdit, startScan, captureAndRecognize, closeModal, runHolderSearch, viewLocationHistory, submitLogin,
+  editHolder, saveHolder, deleteHolder, autofillHolderEmail});
 
 initApp();
